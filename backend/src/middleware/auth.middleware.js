79b23ca566bc0476d021ba4ff35240d6421ceb8c
Garv7-tech <{g04428274@gmail.com}>
2025-05-20 16:10:36 +0530

    feat: initialize LeetLab backend with Prisma, Express, and authentication
    - Added Prisma schema for User and Problem models with enums for UserRole and Difficulty.
    - Implemented Express server with basic routing and middleware for authentication.
    - Created API error and response handling classes for consistent error management.
    - Developed authentication middleware to verify JWT tokens and check user roles.
    - Established routes for user authentication including registration, login, and logout.
    - Added a test data file for problem statements and examples.

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,69 @@
+import jwt from "jsonwebtoken";
+import {db} from "../libs/db.js"
+import {ApiError} from "../libs/apiError.js"
+
+export const authMiddleware = async (req , res , next)=>{
+    try {
+        const token = req.cookies.jwt;
+
+        if(!token){
+            throw new ApiError(401,"Unauthorized - No token provided")
+        }
+
+        let decoded;
+
+        try {
+            decoded = jwt.verify(token , process.env.JWT_SECRET);
+        } catch (error) {
+            throw new ApiError(401,"Unauthorized - Invalid token")
+        }
+
+        const user = await db.user.findUnique({
+            where:{
+                id:decoded.id
+            },
+            select:{
+                id:true,
+                image:true,
+                name:true,
+                email:true,
+                role:true
+            }
+        });
+
+
+        if(!user){
+            throw new ApiError(404,"User not found")
+        }
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        throw new ApiError(500,"Error authenticating user:", error)
+    }
+}
+
+
+export const checkAdmin  = async(req , res , next)=>{
+    try {
+        const userId = req.user.id;
+        
+        const user = await db.user.findUnique({
+            where:{
+                id:userId
+            },
+            select:{
+                role:true
+            }
+        })
+
+        if(!user || user.role !== "ADMIN"){
+            throw new ApiError(403,"Access denied - Admins only")
+        }
+
+        next();
+    } catch (error) {
+        throw new ApiError(500,"Error checking admin role",error)
+    }
+}
